e33b51b2d8d5dcff75024c3faa042fac37e7526b
catcadams <catceciliaadams@gmail.com>
2024-04-29 12:41:55 -0500

    part 1-3 engine/fuel status

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,4 +1,10 @@
 // Declare and initialize the variables for exercise 1 here:
+const engineIndicatorLight = 'red blinking';
+const spaceSuitsOn = true;
+const shuttleCabinReady = true;
+const crewStatus = spaceSuitsOn && shuttleCabinReady;
+const computerStatusCode = 200;
+const shuttleSpeed = 15000;
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
