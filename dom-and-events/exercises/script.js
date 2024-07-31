a6704a50b9717c0ec7a729eb46211351385287a4
catcadams <catceciliaadams@gmail.com>
2024-07-31 13:49:25 -0500

    completed dom exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,27 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+  // Put your code for the exercises here.
+  button.addEventListener("click", function (event) {
+    paragraph.innerHTML = "Houston, we have liftoff!";
+  });
+  missionAbort.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "red";
+  });
+  missionAbort.addEventListener("mouseout", (event) => {
+    event.target.style.backgroundColor = "";
+  });
+  missionAbort.addEventListener("click", (event) => {
+    let confirm = window.confirm("Are you sure you want to abort the mission?");
+    if ((confirm)) {
+      paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+    }
+    else {
+        paragraph.innerHTML;
+    }
+  });
 }
 
 window.addEventListener("load", init);
